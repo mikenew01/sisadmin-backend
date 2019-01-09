1bfd423f5b2cd01e4f08dc1ab449072470a18c78
Maikon Canuto <maikoncanuto@gmail.com>
2019-01-08 22:37:12 -0200

    Configuração heroku

--- src/main/java/io/mk/sisadmin/domain/models/BaseEntity.java
+++ src/main/java/io/mk/sisadmin/domain/models/BaseEntity.java
@@ -3,6 +3,7 @@ package io.mk.sisadmin.domain.models;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 import org.hibernate.Hibernate;
+import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -15,9 +16,12 @@ import java.io.Serializable;
 @NoArgsConstructor
 public class BaseEntity implements Serializable {
 
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "id", unique = true, nullable = false)
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", unique = true, nullable = false)
+    @GeneratedValue(generator = "increment")
+    @GenericGenerator(name = "increment", strategy = "increment")
     private Long id;
 
     @Column(name = "ATIVO")
